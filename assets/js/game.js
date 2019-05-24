6b733d5ad559e6aad60b99922b95d86f1e9ae29d
toomusa <3895499+toomusa@users.noreply.github.com>
2019-05-24 01:28:35 -0700

    progress on validation and display/hide functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -16,18 +16,8 @@ let $guesses = document.getElementById("guesses");
 let $wins = document.getElementById("wins");
 let $losses = document.getElementById("losses");
 let $messageBox = document.getElementById("messageBox");
+let $marquee = document.getElementById("marquee");
 let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-// function tryLettterKeyHandler(e) {
-//     userPick(e.key.toUpperCase());
-//     console.log(userPick);
-// }
-
-
-
-
-
-
 
 var isLetter = function(c) {
     return c.length === 1 && (c.toLowerCase() != c.toUpperCase()); 
@@ -38,9 +28,9 @@ var updateText = function () {
     updateMarquee();
 }
 
-// function displayWord() {
-//     $("#activeWord").textContent = randomWord;
-// }
+function displayWord() {
+    $("#activeWord").textContent = randomWord;
+}
 
 function showPick() {
     $guesses.textContent = userPick;
@@ -50,17 +40,6 @@ function updateMarquee() {
     $("#marquee").textContent = activeWord;
 }
 
-// function startGame() {
-//     var lbl, i;
-//     activeWord = $('#activeWord');
-    
-//     for (i = 0; i < randomWord.length; i++) {
-//         lbl = $('<label/>')
-//                 .text('_')
-//                 .show();
-//         activeWord.append(lbl);
-//     }
-// }
 
 // $(document).ready(function(){
 
@@ -78,6 +57,8 @@ function updateMarquee() {
         }
     }
 
+    // Append to letters guessed if key press is not a match
+
     // when a screen keyboard key is clicked, verify that the key is a letter, change it to upper case, and assign it to userPick
 
     $(".keyboard").on("click", function() {
@@ -91,62 +72,37 @@ function updateMarquee() {
         }
       });
 
-    
-
-
-
-
-
-
-    // function verify () {
-    //     if 
-    // }
-
-      // check to see if userPick has already been selected or if it's a match
-    // document.onkeyup = function(event) {
-    //     let userPick = event.key;
-    //     if (isLetter(userPick)) {
-    //         $messageBox.textContent = userPick;
-    //         if (guesses.indexOf(userPick) < 0) {
-    //             guesses.push(userPick);
-    //             if (rightGuess(userPick)) {
-    //                 attempts--;
-    //                 updateText();
-    //             }
-    //             updateText();
-    //         }
-    //         updateText();
-    //     }
-    //     updateText();
-    // }
-
-    // identify classes and ids
-    // #messageBox
 
     // Split array elements into letters
     for (let i = 0; i < words.length; i++) {
         randomWord = words[Math.floor(Math.random()*words.length)].toUpperCase();
         letters = randomWord.split("");
+        answer = randomWord.split("");
     }
 
-    // Turns letters into underscores
-     for (var n = 0; n < letters.length; n++){
-        letters[n] = "_ ";
-        activeWord = document.getElementById("activeWord");
-        spacer = document.createTextNode(letters[n]);
-        activeWord.appendChild(spacer);
-    if (userPick === letters[n]) {
-    function rightGuess() {
-        for (var n = 0; n < letters.length; n++){
-    activeWord = document.getElementById("activeWord");
-	activeWord.innerHTML=""; 
-	activeWord = document.getElementById("activeWord");
-        spacer = document.createTextNode(letters[n]);
-        activeWord.appendChild(spacer);
-}}
 
+    // Hide display of Words
+    // check to see if userPick has already been selected or if it's a match
+    
+    for (var n = 0; n < answer.length; n++){
+        // debugger;
+        if (userPick === answer[n]) {     
+            $marquee.textContent = userPick; 
+            console.log("yes");    
+            // activeWord = document.getElementById("activeWord");
+            // capture = document.createTextNode(answer[n]);
+            // activeWord.appendChild(capture);
+        } 
+        else {
+            letters[n] = "_ ";
+            activeWord = document.getElementById("activeWord");
+            spacer = document.createTextNode(letters[n]);
+            activeWord.appendChild(spacer);
+        }
     }
-}
+
+
+    // Alternative method???
 
     // for (var i = 0; i < randomWord.length; i++) {
     //     if (userPick === randomWord.charAt(i)) {
@@ -157,23 +113,15 @@ function updateMarquee() {
     // }
 
     console.log(randomWord);
-    // console.log(letters);
+    console.log(letters);
+    console.log(answer);
 
-    // for (let i = 0; i < letters.length; i++) {
-    //     console.log(letters[i]);
-    // }
-
-    // Hide display of Words
-
-    // Listen for user input i.e. keys pressed
 
 
 
     // check key press against Word letters
 
     // Display letter element if key press is a match
-
-    // Append to letters guessed if key press is not a match
 
     // Alert win or loss
 
